ed03554e2e71ec827bb44166cdcf819d046f9b41
anne <richardson.ae@gmail.com>
2015-06-15 20:55:20 -0500

    migrate

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,12 @@
 class Cat < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: cats
+#
+#  id         :integer          not null, primary key
+#  imagefile  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- test/models/cat_test.rb
+++ test/models/cat_test.rb
@@ -5,3 +5,13 @@ class CatTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# == Schema Information
+#
+# Table name: cats
+#
+#  id         :integer          not null, primary key
+#  imagefile  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#

10967dcefdf557b7b50b69284f0134afc60d4443
anne <richardson.ae@gmail.com>
2015-06-15 20:55:07 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Cat.create(imagefile: 'bread.png')
+Cat.create(imagefile: 'ceiling.png')
+Cat.create(imagefile: 'cheezburgerx.png')
+Cat.create(imagefile: 'dodge.png')
+Cat.create(imagefile: 'grumpy.png')
+Cat.create(imagefile: 'hipster.png')
+Cat.create(imagefile: 'hover.png')
+Cat.create(imagefile: 'long.png')
+Cat.create(imagefile: 'serious.png')
+Cat.create(imagefile: 'staring.png')
+
+
